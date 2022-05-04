ccb0fa9acb59b742fe1265706b936bf6d393337e
Eliza Fury <ejfury@protonmail.com>
2022-05-04 21:25:46 +1000

    Change book picture and icon picture

--- src/Context/BookContext.js
+++ src/Context/BookContext.js
@@ -6,13 +6,13 @@ class BookContextProvider extends React.Component {
   state = {
     books: [
       {
-        title: "Kırmızı Pazartesi",
-        author: "Gabriel Garcia Marquez",
-        pageNum: 296,
+        title: "My Year of Rest and Relaxation",
+        author: "Ottessa Moshfegh",
+        pageNum: 288,
         imageURL:
-          "https://i.idefix.com/cache/600x600-0/originals/0000000064101-1.jpg",
+          "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1597676656i/44279110.jpg",
         topic:
-          "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor.",
+          "Psychological Fiction",
       },
     ],
   };
