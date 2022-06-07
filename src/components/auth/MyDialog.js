7157635a9e4433605bb09fc41abe288a249658db
Eliza Fury <ejfury@protonmail.com>
2022-06-07 10:24:54 +1000

    Copy logic from Register button

--- src/components/auth/MyDialog.js
+++ src/components/auth/MyDialog.js
@@ -9,46 +9,35 @@ import Button from "@material-ui/core/Button";
 //Comment
 export default function App() {
 
-  const [open, setOpen] = React.useState(false);
+  const [openModal, setOpenModal] = React.useState('none');
   const [inputValue, setInputValue] = React.useState("");
 
-
-  const setOpenModal = () => {
-    setOpen(true);
-  };
-
-  const handleToClose = () => {
-    setOpen(false);
-  };
-
   return (
-      <div style={{}}>
-        <button onClick={() => setOpenModal('login')}>Login</button>
-        <Dialog open={open} onClose={handleToClose}>
-          <DialogContent>
-            <DialogContentText>
-            <div className="login-wrapper">
-              <h1>Please Log In</h1>
-                <form>
-                  <label>
-                    <p>Username</p>
-                      <input type="text" />
-                  </label>
-                  <label>
-                    <p>Password</p>
-                    <input type="password" />
-                  </label>
-              </form>
-            </div>
-            </DialogContentText>
-          </DialogContent>
-         <DialogActions>
-           <Button onClick={handleToClose} color="primary" autoFocus>
-             Login
-           </Button>
-           <button onClick={() => setOpenModal('none')}>Close</button>
-         </DialogActions>
-       </Dialog>
-     </div>
+    <div style={{}}>
+      <button onClick={() => setOpenModal('login')}>Login</button>
+      <Dialog open={openModal === 'login'}>
+        <DialogContent>
+          <DialogContentText>
+          <div className="login-wrapper">
+            <h1>Register</h1>
+              <form>
+                <label>
+                  <p>Username</p>
+                    <input type="text" />
+                </label>
+                <label>
+                  <p>Password</p>
+                  <input type="password" />
+                </label>
+            </form>
+          </div>
+          </DialogContentText>
+        </DialogContent>
+       <DialogActions>
+         <button onClick={() => setOpenModal('none') === 'login'}>Login</button>
+         <button onClick={() => setOpenModal('none') === 'login'}>Close</button>
+       </DialogActions>
+     </Dialog>
+   </div>
  );
 }
